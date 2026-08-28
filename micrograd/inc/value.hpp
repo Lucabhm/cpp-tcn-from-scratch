ce99e2f184f6e27a15f6b101ee72fa6e889497bc
Lucabhm <lbohm@student.42heilbronn.de>
2026-08-29 01:52:38 +0200

    added operator + and * for Value class also the ability to store the operants for the expresion

--- micrograd/inc/value.hpp
+++ micrograd/inc/value.hpp
@@ -1,19 +1,39 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
 
 class Value {
-	private:
-		double	data;
 	public:
-		Value(double data);
-		void	print_value();
+		double								_data;
+		std::vector<std::shared_ptr<Value>>	_prev;
+		std::string							_op;
+		std::string							_label;
+
+		Value(double data, std::string label = "", std::vector<std::shared_ptr<Value>> children = {}, std::string op = "");
+
+		static std::shared_ptr<Value>	create(double data, std::string label);
 };
 
-Value::Value(double data) {
-	this->data = data;
+using	ValuePtr = std::shared_ptr<Value>;
+
+Value::Value(double data, std::string label, std::vector<std::shared_ptr<Value>> children, std::string op)
+	: _data(data), _prev(std::move(children)), _op(std::move(op)), _label(std::move(label)) {}
+
+ValuePtr	Value::create(double data, std::string label) {
+	return std::make_shared<Value>(data, label);
 }
 
-void	Value::print_value() {
-	std::cout << this->data << std::endl;
+ValuePtr	operator+(const ValuePtr&a, const ValuePtr &b) {
+	auto	out = std::make_shared<Value>(a->_data + b->_data, "", std::vector<ValuePtr>{a, b}, "+");
+
+	return out;
+}
+
+ValuePtr	operator*(const ValuePtr &a, const ValuePtr &b) {
+	auto	out = std::make_shared<Value>(a->_data * b->_data, "", std::vector<ValuePtr>{a, b}, "*");
+
+	return out;
 }
--- micrograd/src/main.cpp
+++ micrograd/src/main.cpp
@@ -1,8 +1,32 @@
 #include "value.hpp"
 
-int	main() {
-	Value	test(5.0);
+void	print_graph(ValuePtr input);
 
-	test.print_value();
+int	main() {
+	ValuePtr	a = Value::create(2.0, "a");
+	ValuePtr	b = Value::create(-3.0, "b");
+	ValuePtr	c = Value::create(10.0, "c");
+	ValuePtr	d = a * b + c;
+	ValuePtr	e = d + a;
+
+	d->_label = "d";
+	e->_label = "e";
+
+	print_graph(e);
+
 	return 0;
+}
+
+void	print_graph(ValuePtr input) {
+	if (input->_prev.empty())
+		return;
+
+	std::cout << "label = " << input->_label << std::endl;
+	std::cout << "data = " << input->_data << std::endl;
+	for (int i{0}; i < input->_prev.size(); ++i) {
+		std::cout << "Expresion created by " << input->_prev.at(i)->_label << std::endl;
+	}
+	std::cout << "with operator " << input->_op << std::endl;
+
+	print_graph(input->_prev.at(0));
 }
