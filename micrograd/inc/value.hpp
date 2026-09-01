b777d30cd9dde5e6947f9091e4d8a579736f77a9
Lucabhm <lbohm@student.42heilbronn.de>
2026-09-01 23:06:55 +0200

    added backpropagation for +, * and tanh

--- micrograd/inc/value.hpp
+++ micrograd/inc/value.hpp
@@ -4,10 +4,14 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <functional>
+#include <cmath>
 
-class Value {
+class Value : public std::enable_shared_from_this<Value> {
 	public:
 		double								_data;
+		double								_grad{};
+		std::function<void()>				_backward = []() {};
 		std::vector<std::shared_ptr<Value>>	_prev;
 		std::string							_op;
 		std::string							_label;
@@ -15,6 +19,7 @@ class Value {
 		Value(double data, std::string label = "", std::vector<std::shared_ptr<Value>> children = {}, std::string op = "");
 
 		static std::shared_ptr<Value>	create(double data, std::string label);
+		std::shared_ptr<Value>			tanh();
 };
 
 using	ValuePtr = std::shared_ptr<Value>;
@@ -29,11 +34,33 @@ ValuePtr	Value::create(double data, std::string label) {
 ValuePtr	operator+(const ValuePtr&a, const ValuePtr &b) {
 	auto	out = std::make_shared<Value>(a->_data + b->_data, "", std::vector<ValuePtr>{a, b}, "+");
 
+	out->_backward = [a, b, out]() {
+		a->_grad += out->_grad;
+		b->_grad += out->_grad;
+	};
+
 	return out;
 }
 
 ValuePtr	operator*(const ValuePtr &a, const ValuePtr &b) {
 	auto	out = std::make_shared<Value>(a->_data * b->_data, "", std::vector<ValuePtr>{a, b}, "*");
+
+	out->_backward = [a, b, out]() {
+		a->_grad = b->_data * out->_grad;
+		b->_grad = a->_data * out->_grad;
+	};
+
+	return out;
+}
+
+std::shared_ptr<Value>	Value::tanh() {
+	double	x = this->_data;
+	double	t = (std::exp(2.0 * x) - 1.0) / (std::exp(2.0 * x) + 1);
+	auto	out = std::make_shared<Value>(t, "tanh", std::vector<ValuePtr>{shared_from_this()});
+
+	out->_backward = [this, t, out]() {
+		this->_grad = (1.0 - std::pow(t, 2)) * out->_grad;
+	};
 
 	return out;
 }
--- micrograd/src/main.cpp
+++ micrograd/src/main.cpp
@@ -1,28 +1,32 @@
 #include "value.hpp"
 
-void	print_graph(ValuePtr input);
+void	print_graph(std::shared_ptr<Value> &input);
 
 int	main() {
-	ValuePtr	a = Value::create(2.0, "a");
-	ValuePtr	b = Value::create(-3.0, "b");
-	ValuePtr	c = Value::create(10.0, "c");
-	ValuePtr	d = a * b + c;
-	ValuePtr	e = d + a;
+	ValuePtr	x1 = Value::create(2.0, "x1");
+	ValuePtr	x2 = Value::create(0.0, "x1");
+	ValuePtr	w1 = Value::create(-3.0, "w1");
+	ValuePtr	w2 = Value::create(1.0, "w2");
+	ValuePtr	b = Value::create(6.88, "b");
+	ValuePtr	x1w1 = x1 * w1; x1w1->_label = "x1w1";
+	ValuePtr	x2w2 = x2 * w2; x2w2->_label = "x2w2";
+	ValuePtr	x1w1x2w2 = x1w1 + x2w2; x1w1x2w2->_label = "x1w1x2w2";
+	ValuePtr	n = x1w1x2w2 + b; n->_label = "n";
+	ValuePtr	o = n->tanh(); o->_label = "o";
 
-	d->_label = "d";
-	e->_label = "e";
-
-	print_graph(e);
-
-	return 0;
+	o->_grad = 1.0;
+	o->_backward();
+	n->_backward();
+	print_graph(o);
 }
 
-void	print_graph(ValuePtr input) {
+void	print_graph(std::shared_ptr<Value> &input) {
 	if (input->_prev.empty())
 		return;
 
 	std::cout << "label = " << input->_label << std::endl;
 	std::cout << "data = " << input->_data << std::endl;
+	std::cout << "grad = " << input->_grad << std::endl;
 	for (int i{0}; i < input->_prev.size(); ++i) {
 		std::cout << "Expresion created by " << input->_prev.at(i)->_label << std::endl;
 	}
